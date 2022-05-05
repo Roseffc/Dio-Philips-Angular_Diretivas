92e22815778f481c48f6033cd0e307c1ce05d945
Roseffc <roseffc@gmail.com>
2022-05-05 10:38:42 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'diretivasangular';
+export class AppComponent implements OnInit {
+  count = 0;
+  // para ler a variavel abaixo - ver component.html
+  nome = 'Rosangela Freitas Ferreira Cardoso';
+  // two e data base
+  text = '';
+
+
+  pessoas = [
+    {
+      nome: "Rosangela",
+      profissao: "Desenvolvedora Fron-End"
+    },
+    {
+      nome: "Messias",
+      profissao: "Desenvolvedor Fron-End"
+    },
+    {
+      nome: "Ellen",
+      profissao: "Médica"
+    },
+    {
+      nome: "Gabriel",
+      profissao: "Engenheiro Químico"
+    }
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log(this.pessoas);
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10){
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  // para passar informações do html para a logica 
+
+  clicou(nome: string): void{
+    console.log('clicou em min', nome)
+  }
 }
